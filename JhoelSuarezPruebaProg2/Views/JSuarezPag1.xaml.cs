1accc017847146c1ceb5c0de3e51821cee5550ff
jourt1 <jhoelsuarez02@gmail.com>
2024-12-03 07:26:47 -0500

    Creacion de las dos pantallas

---
+++ JhoelSuarezPruebaProg2/Views/JSuarezPag1.xaml.cs
@@ -0,0 +1,9 @@
+namespace JhoelSuarezPruebaProg2.Views;
+
+public partial class JSuarezPag1 : ContentPage
+{
+	public JSuarezPag1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ JhoelSuarezPruebaProg2/Views/JSuarezPag2.xaml.cs
@@ -0,0 +1,9 @@
+namespace JhoelSuarezPruebaProg2.Views;
+
+public partial class JSuarezPag2 : ContentPage
+{
+	public JSuarezPag2()
+	{
+		InitializeComponent();
+	}
+}
